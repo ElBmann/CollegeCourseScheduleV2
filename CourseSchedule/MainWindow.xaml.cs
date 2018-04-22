df30eaa38dc1142a47e713155002e4d1a834f3b3
brianrecuero <brianrecuero@gmail.com>
2018-04-22 14:57:51 -0400

    Added Regex for Coourse ID so only numbers can be placed in the text field, Updated the open Course collection so it pulls the data from the JSON file. Also added an is empty function looking for empty strings to avoid application from crashing.

--- CourseSchedule/MainWindow.xaml.cs
+++ CourseSchedule/MainWindow.xaml.cs
@@ -14,106 +14,154 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CollegeCourseLibrary1;
 using Microsoft.Win32;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text.RegularExpressions;
 
 namespace CourseSchedule
 {
-   
+
     public partial class MainWindow : Window
     {
         DataLayer DL = new DataLayer();
         CourseCollection CC = new CourseCollection();
         Course c = new Course();
         OpenFileDialog OFD = new OpenFileDialog();
+        string path = @"C:\Users\Brian Recuero\Source\Repos\CollegeCourseScheduleV2\CourseSchedule\bin";
 
-        
         public MainWindow()
         {
             InitializeComponent();
             _CourseFileNameTextBox.IsReadOnly = true;
-            OFD.InitialDirectory = @"C:\Users\Brian Recuero\Source\Repos\CollegeCourseScheduleV2\CourseSchedule\bin";
+            OFD.InitialDirectory = path;
             //DataLayer DL;
         }
 
         private void _FindCourseIDButton_Click(object sender, RoutedEventArgs e)
         {
-            string C_ID = "";
-            C_ID = _FindCourseIDTextBox.Text;
-            int CourseID = Int32.Parse(C_ID);
-            
 
-            c.Id = CourseID;
-            c.Number = "23";
-            c.Title = "Hello";
-            c.Credits = 23;
-            c.Description = "Hello there";
-            c.Designator = "Balls";
+
+         
+            if (isEmptyFindCourseID())
+            {
+                MessageBox.Show("Please enter courseID");
+            }
+            else
+            {
+                string C_ID = "";
+                C_ID = _FindCourseIDTextBox.Text;
+                int CourseID = Int32.Parse(C_ID);
+                c.Id = CourseID;
+                c.Number = "23";
+                c.Title = "Hello";
+                c.Credits = 23;
+                c.Description = "Hello there";
+                c.Designator = "Balls";
+
+
+
+
+                CC.CList.Add(c);
+
+                if (CC.Find(CourseID) != null)
+                {
+                    // Grabing data from Course Collection List. And populating the TextBox in Course Data
+                    _CourseIDDataTextBox.Text = c.Id.ToString();
+                    _DesignatorDataTextBox.Text = c.Designator.ToString();
+                    _NumberDataTextBox.Text = c.Number.ToString();
+                    _TitleDataTextBox.Text = c.Title.ToString();
+                    _CreditsDataTextBox.Text = c.Credits.ToString();
+                    _DescriptionDataTextBox.Text = c.Description.ToString();
+
+
+
+
+                    MessageBox.Show(c.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("Could Not Find");
+
+                }
+            }
 
            
 
 
-            CC.CList.Add(c);
-
-            if (CC.Find(CourseID) !=null)
-            {
-                // Grabing data from Course Collection List. And populating the TextBox in Course Data
-               _CourseIDDataTextBox.Text = c.Id.ToString();
-               _DesignatorDataTextBox.Text = c.Designator.ToString();
-               _NumberDataTextBox.Text = c.Number.ToString();
-               _TitleDataTextBox.Text = c.Title.ToString();
-               _CreditsDataTextBox.Text = c.Credits.ToString();
-               _DescriptionDataTextBox.Text = c.Description.ToString();
-
-                /*  //Reads Course Collection from a JSON file
-                            CourseCollection cmc = new CourseCollection();
-
-                            Console.WriteLine("*********************");
-                            Console.WriteLine("Read CourseCollection from JSON file");
-                            Console.WriteLine("*********************");
-
-                            Console.Write("Enter a File Name: ");
-                            string courseReadFile = Console.ReadLine() + ".json";
-                            //Opens the file stream to read the JSON
-                            FileStream courseReader = new FileStream(courseReadFile, FileMode.Open, FileAccess.Read);
-                            //DataContractJsonSerializer Instance
-                            DataContractJsonSerializer inputCourseSerializer;
-                            inputCourseSerializer = new DataContractJsonSerializer(typeof(CourseCollection));
-
-                            cCollection = (CourseCollection)inputCourseSerializer.ReadObject(courseReader);
-                            courseReader.Close();
-
-                            Console.WriteLine("CourseCollection JSON Read \n");
-
-
-                            returnToMenu = true;
-                            break;*/
-
-
-
-                MessageBox.Show(c.ToString());
-            }
-            else
-            {
-                MessageBox.Show("Could Not Find");
-                
-            }
-                 
-            
-            }
+        }
 
         private void _OpenCrsCollectionbutton_Click(object sender, RoutedEventArgs e)
         {
-          
+            //Clear the text boxs
+            _FindCourseIDTextBox.Clear();
+            _FindDesignatorTextBox.Clear();
+            _FindNumberTextBox.Clear();
+            _CourseIDDataTextBox.Clear();
+            _CreditsDataTextBox.Clear();
+            _DesignatorDataTextBox.Clear();
+            _DescriptionDataTextBox.Clear();
+            _FindCourseIDTextBox.Clear();
+            _NumberDataTextBox.Clear();
+            _TitleDataTextBox.Clear();
+
+
+
+
+
             OFD.DefaultExt = ".json";
             OFD.Filter = "Json Document (.json)|*.json";
             if (OFD.ShowDialog() == true)
             {
                 string filename = OFD.FileName;
                 _CourseFileNameTextBox.Text = filename;
-            }
-               
-            {
+
+                //Opens the file stream to read the JSON
+                FileStream courseReader = new FileStream(filename, FileMode.Open, FileAccess.Read);
+
+                //DataContractJsonSerializer Instance
+                DataContractJsonSerializer inputCourseSerializer;
+
+                inputCourseSerializer = new DataContractJsonSerializer(typeof(CourseCollection));
+
+                CC = (CourseCollection)inputCourseSerializer.ReadObject(courseReader);
+
+                //This can be used to populate fields or list views
+               /* foreach (var listCourses in CC.CList)
+                {
+                    _CourseIDDataTextBox.Text = listCourses.Id.ToString();
+                }*/
+                courseReader.Close();
 
             }
+
+           
+
+               Console.WriteLine("CourseCollection JSON Read \n");
+
+             
+
+            
+        }//End
+
+        public bool isEmptyFindCourseID()
+        {
+            if (_FindCourseIDTextBox.Text == "")
+            {
+                return true;
+            }
+            return false;
+        }//End isEmpty 
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+            if (e.Handled == true)
+            {
+                MessageBox.Show("Please enter numbers Only");
+            }
         }
+
+      
     }
-    }
+}
