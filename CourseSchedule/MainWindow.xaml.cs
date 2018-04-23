0e64d95a13743cee6bcc94632724f47fa2bf6e50
brianrecuero <brianrecuero@gmail.com>
2018-04-22 20:10:11 -0400

    Updated the find method. It grabs the data in the Json File then searchs through for the correct Course ID.

--- CourseSchedule/MainWindow.xaml.cs
+++ CourseSchedule/MainWindow.xaml.cs
@@ -39,44 +39,93 @@ namespace CourseSchedule
 
         private void _FindCourseIDButton_Click(object sender, RoutedEventArgs e)
         {
-         
+            string C_ID = "";
+            string C_Num = "";
+            string C_Title = "";
+            string C_Credits = "";
+            string C_Desc = "";
+            string C_Desi = "";
+            string searchID = "";
+
+            _CourseIDDataTextBox.Clear();
+            _NumberDataTextBox.Clear();
+            _TitleDataTextBox.Clear();
+            _CreditsDataTextBox.Clear();
+            _DescriptionDataTextBox.Clear();
+            _DesignatorDataTextBox.Clear();
+
             if (isEmptyFindCourseID())
             {
                 MessageBox.Show("Please enter courseID");
             }
             else
             {
-                string C_ID = "";
-                C_ID = _FindCourseIDTextBox.Text;
-                int CourseID = Int32.Parse(C_ID);
-                c.Id = CourseID;
-                c.Number = "23";
-                c.Title = "Hello";
-                c.Credits = 23;
-                c.Description = "Hello there";
-                c.Designator = "Balls";
+                
+                string filename = "courses.json";
+
+                      //Opens the file stream to read the JSON
+                FileStream courseReader = new FileStream(filename, FileMode.Open, FileAccess.Read);
+
+                //DataContractJsonSerializer Instance
+                DataContractJsonSerializer inputCourseSerializer;
+                inputCourseSerializer = new DataContractJsonSerializer(typeof(CourseCollection));
+
+                //Populate the CourseCollection window member variable with data from the selected file (deserialize into that member variable). 
+                CC = (CourseCollection)inputCourseSerializer.ReadObject(courseReader);
+
+                //This can be used to populate fields or list views
+                foreach (var listCourses in CC.CList)
+                {
+                    C_ID = listCourses.Id.ToString();
+                    C_Num = listCourses.Number.ToString();
+                    C_Title = listCourses.Title.ToString();
+                    C_Credits = listCourses.Credits.ToString();
+                    C_Desc = listCourses.Description.ToString();
+                    C_Desi = listCourses.Designator.ToString();
+                    //  _CourseIDDataTextBox.Text = listCourses.Id.ToString();
+
+                  
+                   
+                    int credits = Int32.Parse(C_Credits);
+                    int course_ID = Int32.Parse(C_ID);
+                    c.Id = course_ID;
+                    c.Number = C_Num;
+                    c.Title = C_Title;
+                    c.Credits = credits;
+                    c.Description = C_Desc;
+                    c.Designator = C_Desi;
+
+
+                }
 
 
                 CC.CList.Add(c);
 
-                if (CC.Find(CourseID) != null)
+                //Converts the sting into an int
+                searchID = _FindCourseIDTextBox.Text;
+                int searchCourseID = Int32.Parse(searchID);
+
+
+                //If Id is null the course does not exsis
+                if (CC.Find(searchCourseID) != null)
                 {
                     // Grabing data from Course Collection List. And populating the TextBox in Course Data
-                    _CourseIDDataTextBox.Text = c.Id.ToString();
-                    _DesignatorDataTextBox.Text = c.Designator.ToString();
-                    _NumberDataTextBox.Text = c.Number.ToString();
-                    _TitleDataTextBox.Text = c.Title.ToString();
-                    _CreditsDataTextBox.Text = c.Credits.ToString();
-                    _DescriptionDataTextBox.Text = c.Description.ToString();
+                    _CourseIDDataTextBox.Text = CC.Find(searchCourseID).Id.ToString();
+                    _DesignatorDataTextBox.Text = CC.Find(searchCourseID).Designator.ToString();
+                    _NumberDataTextBox.Text = CC.Find(searchCourseID).Number.ToString();
+                    _TitleDataTextBox.Text = CC.Find(searchCourseID).Title.ToString();
+                    _CreditsDataTextBox.Text = CC.Find(searchCourseID).Credits.ToString();
+                    _DescriptionDataTextBox.Text = CC.Find(searchCourseID).Description.ToString();
 
 
-                    MessageBox.Show(c.ToString());
+                   // MessageBox.Show(c.ToString());
                 }
                 else
                 {
                     MessageBox.Show("Could Not Find");
 
                 }
+                courseReader.Close();
             }
 
 
