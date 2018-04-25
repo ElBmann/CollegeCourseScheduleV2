93589e114ebd69f9d69ed17910407ac71d5a41c4
brianrecuero <brianrecuero@gmail.com>
2018-04-25 18:52:28 -0400

    Added connection to ListView  for the Professor Collections

--- CourseSchedule/MainWindow.xaml.cs
+++ CourseSchedule/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace CourseSchedule
     {
         DataLayer DL = new DataLayer();
         CourseCollection CC = new CourseCollection();
+        ProfessorCollection PC = new ProfessorCollection();
         Course c = new Course();
         OpenFileDialog OFD = new OpenFileDialog();
         string path = @"C:\Users\Brian Recuero\Source\Repos\CollegeCourseScheduleV2\CourseSchedule\bin";
@@ -183,44 +184,93 @@ namespace CourseSchedule
 
         private void _FindDesignatorAndNumberButton_Click(object sender, RoutedEventArgs e)
         {
+            string C_ID = "";
+            string C_Num = "";
+            string C_Title = "";
+            string C_Credits = "";
+            string C_Desc = "";
+            string C_Desi = "";
+            string searchID = "";
+
+            _CourseIDDataTextBox.Clear();
+            _NumberDataTextBox.Clear();
+            _TitleDataTextBox.Clear();
+            _CreditsDataTextBox.Clear();
+            _DescriptionDataTextBox.Clear();
+            _DesignatorDataTextBox.Clear();
+
             if (isEmptyFindDesAndNumber())
             {
-                MessageBox.Show("Please enter Designator and Number");
+                MessageBox.Show("Please enter courseID");
             }
             else
             {
-                string C_Des = "";
-                string C_Num = "";
-                C_Des = _FindDesignatorTextBox.Text;
-                C_Num = _FindNumberTextBox.Text;
-                c.Id = 23;
-                c.Number = C_Num;
-                c.Title = "Hello";
-                c.Credits = 23;
-                c.Description = "Hello there";
-                c.Designator = C_Des;
+
+                string filename = "courses.json";
+
+                //Opens the file stream to read the JSON
+                FileStream courseReader = new FileStream(filename, FileMode.Open, FileAccess.Read);
+
+                //DataContractJsonSerializer Instance
+                DataContractJsonSerializer inputCourseSerializer;
+                inputCourseSerializer = new DataContractJsonSerializer(typeof(CourseCollection));
+
+                //Populate the CourseCollection window member variable with data from the selected file (deserialize into that member variable). 
+                CC = (CourseCollection)inputCourseSerializer.ReadObject(courseReader);
+
+                //This can be used to populate fields or list views
+                foreach (var listCourses in CC.CList)
+                {
+                    C_ID = listCourses.Id.ToString();
+                    C_Num = listCourses.Number.ToString();
+                    C_Title = listCourses.Title.ToString();
+                    C_Credits = listCourses.Credits.ToString();
+                    C_Desc = listCourses.Description.ToString();
+                    C_Desi = listCourses.Designator.ToString();
+                    //  _CourseIDDataTextBox.Text = listCourses.Id.ToString();
+
+
+
+                    int credits = Int32.Parse(C_Credits);
+                    int course_ID = Int32.Parse(C_ID);
+                    c.Id = course_ID;
+                    c.Number = C_Num;
+                    c.Title = C_Title;
+                    c.Credits = credits;
+                    c.Description = C_Desc;
+                    c.Designator = C_Desi;
+
+
+                }
 
 
                 CC.CList.Add(c);
 
-                if (CC.Find(C_Des, C_Num) != null)
+                //Converts the sting into an int
+            //    searchID = _FindCourseIDTextBox.Text;
+            //    int searchCourseID = Int32.Parse(searchID);
+
+
+                //If Id is null the course does not exsis
+                if (CC.Find(_FindDesignatorTextBox.Text, _FindNumberTextBox.Text) != null)
                 {
                     // Grabing data from Course Collection List. And populating the TextBox in Course Data
-                    _CourseIDDataTextBox.Text = c.Id.ToString();
-                    _DesignatorDataTextBox.Text = c.Designator.ToString();
-                    _NumberDataTextBox.Text = c.Number.ToString();
-                    _TitleDataTextBox.Text = c.Title.ToString();
-                    _CreditsDataTextBox.Text = c.Credits.ToString();
-                    _DescriptionDataTextBox.Text = c.Description.ToString();
+                    _CourseIDDataTextBox.Text = CC.Find(_FindDesignatorTextBox.Text, _FindNumberTextBox.Text).Id.ToString();
+                    _DesignatorDataTextBox.Text = CC.Find(_FindDesignatorTextBox.Text, _FindNumberTextBox.Text).Designator.ToString();
+                    _NumberDataTextBox.Text = CC.Find(_FindDesignatorTextBox.Text, _FindNumberTextBox.Text).Number.ToString();
+                    _TitleDataTextBox.Text = CC.Find(_FindDesignatorTextBox.Text, _FindNumberTextBox.Text).Title.ToString();
+                    _CreditsDataTextBox.Text = CC.Find(_FindDesignatorTextBox.Text, _FindNumberTextBox.Text).Credits.ToString();
+                    _DescriptionDataTextBox.Text = CC.Find(_FindDesignatorTextBox.Text, _FindNumberTextBox.Text).Description.ToString();
 
 
-                    MessageBox.Show(c.ToString());
+                    // MessageBox.Show(c.ToString());
                 }
                 else
                 {
                     MessageBox.Show("Could Not Find");
 
                 }
+                courseReader.Close();
             }
         }// End _FindDesignatorAndNumberButton_Click
 
@@ -252,7 +302,54 @@ namespace CourseSchedule
             }
         }//End NumberValidationTextBox
 
-     
+        private void _ProfessorCollectionButton_Click(object sender, RoutedEventArgs e)
+        {
 
+            //Clear the text boxs Once Searching for the JSON file
+            _FindCourseIDTextBox.Clear();
+            _FindDesignatorTextBox.Clear();
+            _FindNumberTextBox.Clear();
+            _CourseIDDataTextBox.Clear();
+            _CreditsDataTextBox.Clear();
+            _DesignatorDataTextBox.Clear();
+            _DescriptionDataTextBox.Clear();
+            _FindCourseIDTextBox.Clear();
+            _NumberDataTextBox.Clear();
+            _TitleDataTextBox.Clear();
+
+
+
+            //Setting up the default type once you open the file dialog 
+
+            OFD.DefaultExt = ".json";
+            OFD.Filter = "Json Document (.json)|*.json";
+            OFD.Title = "Open Course Collection From JSON";
+            if (OFD.ShowDialog() == true)
+            {
+                string filename = OFD.FileName;
+                _ProfessorsFileNameTextBox.Text = filename;
+
+                //Opens the file stream to read the JSON
+                FileStream courseReader = new FileStream(filename, FileMode.Open, FileAccess.Read);
+
+                //DataContractJsonSerializer Instance
+                DataContractJsonSerializer inputCourseSerializer;
+                inputCourseSerializer = new DataContractJsonSerializer(typeof(ProfessorCollection));
+
+                //Populate the CourseCollection window member variable with data from the selected file (deserialize into that member variable). 
+                PC = (ProfessorCollection)inputCourseSerializer.ReadObject(courseReader);
+
+                //This can be used to populate fields or list views
+                 foreach (var PList in PC.PList)
+                 {
+                    listView.Items.Add(PList);
+                    
+                 }
+                courseReader.Close();
+
+            }
+
+
+        }
     }//End Main Window Class 
 }
